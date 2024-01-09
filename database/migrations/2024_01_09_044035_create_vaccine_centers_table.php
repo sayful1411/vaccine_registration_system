fcd72373b2b6d891f2b6285115abd1c5b9a6a921
Md Sayful Islam <mdsayfulislam1411@gmail.com>
2024-01-09 11:11:21 +0600

    update vaccine center migration

--- database/migrations/2024_01_09_044035_create_vaccine_centers_table.php
+++ database/migrations/2024_01_09_044035_create_vaccine_centers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('vaccine_centers', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
+            $table->string('name', 50)->unique();
             $table->unsignedInteger('daily_limit')->default(10);
             $table->timestamps();
         });
